8f6db72ea353dd50ac0df7522a46267c61f90cb0
Protyusa <143247237+Protyusa@users.noreply.github.com>
2026-01-07 14:00:08 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React Demo App</h1>
+      <p>Welcome to CI/CD Pipeline</p>
+    </div>
+  );
+}
+
+export default App;
